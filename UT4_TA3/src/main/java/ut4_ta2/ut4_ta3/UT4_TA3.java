3b394a40e89ed37e4f0649acaeb3e17e6084bd09
Alejandra2431 <maria.sosadias@correo.ucu.edu.uy>
2022-05-11 21:09:48 -0300

    .

---
+++ UT4_TA3/src/main/java/ut4_ta2/ut4_ta3/UT4_TA3.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package ut4_ta2.ut4_ta3;
+
+/**
+ *
+ * @author Administrador
+ */
+public class UT4_TA3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
