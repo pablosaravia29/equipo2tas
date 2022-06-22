e0a2e820e4314c60043d844dc1d9859eca85f5b7
Levi de los Santos <levidlsantos@gmail.com>
2022-06-22 11:15:14 -0300

    correcciones

--- ut5abo_ta15/src/main/java/ucu/edu/uy/ut5abo/Main.java
+++ ut5abo_ta15/src/main/java/ucu/edu/uy/ut5abo/Main.java
@@ -11,36 +11,37 @@ import ucu.edu.uy.util.ManejadorArchivosGenerico;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class Main
-{
+public class Main {
 
-    public static void main(String[] args)
-    {
-        String[] palabras = ManejadorArchivosGenerico.leerArchivo("palabras.txt");
+    public static void main(String[] args) {
         TArbolBB arbolPalabras = new TArbolBB();
         ArrayList<String> lista = new ArrayList<>();
+        String[] palabras = ManejadorArchivosGenerico.leerArchivo("palabras.txt");
         for (String lineas : palabras) {
-            if(lineas.length() == 2)
-                try {
-                    String[] palabra = lineas.split(" ");
-                    lista.add(palabra[0]);
-                } catch (Exception e) {
-                    System.out.println("Error al cargar la linea del archivo");
-                }
-        } 
+            String[] palabra = lineas.split(" ");
+            if (palabra.length == 2)
+             try {
+                lista.add(palabra[0]);
+                System.out.println(palabra[0]);
+            } catch (Exception e) {
+                System.out.println("Error al cargar la linea del archivo");
+                continue;
+            }
+        }
 
         Collections.shuffle(lista);
-        for(String s: lista) {
+        for (String s : lista) {
             arbolPalabras.insertar(new TElementoAB(s, s));
 
         }
         Lista<String> temp = new Lista<>();
         temp = arbolPalabras.listaDatosNivelMasProfundo();
         Nodo<String> aux = temp.getPrimero();
-        while (aux != null){
+        while (aux != null) {
             System.out.println(aux.getEtiqueta());
+            aux = aux.getSiguiente();
         }
-        
+
         int resultado = 0;
         resultado = arbolPalabras.Iti();
         System.out.println("El resultado de longitud de trayectoria interna es: " + resultado);
